9d44d0e2c45fe1662f4417abff1cf01709051222
tinayiluo0322 <143360909+tinayiluo0322@users.noreply.github.com>
2023-09-06 15:28:51 -0400

    revise test 2

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ from main import readfile
 
 
 def test_readfile():
-    file_path = "/Users/tinayiluo/Desktop/Duke_23_Fall/Data_Engineering/emails.csv"
+    file_path = "/Users/tinayiluo/Desktop/emails.csv"
     result = readfile(file_path)
     print(result)
 
