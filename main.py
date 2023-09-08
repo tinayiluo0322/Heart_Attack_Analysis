065b359227754b531c593caee6de45bd97a360c2
tinayiluo0322 <143360909+tinayiluo0322@users.noreply.github.com>
2023-09-08 13:30:00 -0400

    revise test cases

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ Main code
 import pandas as pd
 
 
+# generates summary statistics for the numeric columns in a DataFrame.
 def readfile(file_path):
     data = pd.read_csv(file_path)
     summary_stats = data.describe()
--- test_main.py
+++ test_main.py
@@ -5,10 +5,19 @@ Test goes here
 from main import readfile
 
 
+# test cases
 def test_readfile():
     file_path = "emails.csv"
     result = readfile(file_path)
-    print(result)
+    # mean value of first column in emails.csv
+    mean_first_column = result.iloc[:, 0]["mean"]
+    assert mean_first_column == 6.640564578499613
+    # min value of first column in emails.csv
+    min_first_column = result.iloc[:, 0]["min"]
+    assert min_first_column == 0.0
+    # max value of first column in emails.csv
+    max_first_column = result.iloc[:, 0]["max"]
+    assert max_first_column == 210.0
 
 
 if __name__ == "__main__":
